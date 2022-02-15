7d5bd4c51ec6c77c0253e307f07f9470ddbc529e
Chris <34993144+Mesiow@users.noreply.github.com>
2022-02-14 22:27:36 -0500

    dont need to be super accurate just tick components by cpu cycles

--- BlissSMS/Core/System.c
+++ BlissSMS/Core/System.c
@@ -16,19 +16,13 @@ void systemRunEmulation(struct System* sys)
 	if (sys->running) {
 		s32 cycles_this_frame = 0;
 		while (cycles_this_frame < MAX_CYCLES_PER_FRAME) {
-			u16 cpu_cycles = z80Clock(&sys->z80);
+			u16 cycles = z80Clock(&sys->z80);
 
-			//the main system clock is 3x faster than the z80 clock
-			s32 machine_cycles = cpu_cycles * 3;
-			//vdp clock is half the speed of the main system clock
-			s32 vdp_cycles = machine_cycles / 2;
+			//vdp.update(cycles);
+			//psg.update(cycles);
+			//z80HandleInterrupts(&sys->z80)
 
-			//tick timers
-			//tick vdp (graphics unit)
-			//tick sound unit
-			//check for interrupts
-
-			cycles_this_frame += machine_cycles;
+			cycles_this_frame += cycles;
 		}
 		//update joypad
 		//update graphics buffer
