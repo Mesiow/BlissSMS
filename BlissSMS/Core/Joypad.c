0a9d41e3b96f8558328bf16d76285ecef255a1cf
Chris <34993144+Mesiow@users.noreply.github.com>
2022-03-25 22:09:56 -0400

    fix joypad released bug

--- BlissSMS/Core/Joypad.c
+++ BlissSMS/Core/Joypad.c
@@ -26,12 +26,12 @@ void joypadButtonPressed(struct Joypad* joy, enum Button btn, u8 pressed)
 	}
 	else {
 		switch (btn) {
-			case Up: joy->joypad_temp ^= ~(BIT_UP); break;
-			case Down: joy->joypad_temp ^= ~(BIT_DOWN); break;
-			case Left: joy->joypad_temp ^= ~(BIT_LEFT); break;
-			case Right: joy->joypad_temp ^= ~(BIT_RIGHT); break;
-			case A: joy->joypad_temp ^= ~(BIT_A); break;
-			case B: joy->joypad_temp ^= ~(BIT_B); break;
+			case Up: joy->joypad_temp ^= (BIT_UP); break;
+			case Down: joy->joypad_temp ^= (BIT_DOWN); break;
+			case Left: joy->joypad_temp ^= (BIT_LEFT); break;
+			case Right: joy->joypad_temp ^= (BIT_RIGHT); break;
+			case A: joy->joypad_temp ^= (BIT_A); break;
+			case B: joy->joypad_temp ^= (BIT_B); break;
 		}
 	}
 }
