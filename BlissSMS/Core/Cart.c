9e0885ee311422ff04f35bfe37fd4618f06dd71e
Chris <34993144+Mesiow@users.noreply.github.com>
2022-04-03 20:08:29 -0400

    detect sram

--- BlissSMS/Core/Cart.c
+++ BlissSMS/Core/Cart.c
@@ -3,43 +3,74 @@
 void cartInit(struct Cart* cart)
 {
 	cart->memory = NULL;
+	cart->uses_sram = 0;
 	memset(cart->ram_banks, 0x0, 0x8000);
-	memset(cart->sram, 0x0, SRAM_SIZE);
 }
 
-void cartLoad(struct Cart* cart, const char* path)
+void cartLoad(struct Cart* cart, char* path)
 {
-	FILE* rom = fopen(path, "r");
-	if (rom == NULL) {
-		printf("---Cartridge file: %s could not be found---\n", path);
-		return;
-	}
-	//Determine rom size
-	fseek(rom, 0, SEEK_END);
-	u32 file_size = ftell(rom);
-	fseek(rom, 0, SEEK_SET);
+	char* ext = ".sms";
+	s32 str_size = strlen(path) + strlen(ext) + 1;
 
-	cart->romsize = file_size;
-	//32kb, 64kb, 128kb, 256kb
-	if (file_size == CART_32K || file_size == CART_64K
-		|| file_size == CART_128K || file_size == CART_256K
-		|| file_size == CART_512K) {
-		cart->memory = (u8*)malloc(file_size * sizeof(u8));
-	}
-	else {
-		printf("--Cartridge size: 0x%05X not supported!--\n", file_size);
-		return;
-	}
+	char* dest = (u8*)malloc(str_size);
+	if (dest != NULL) {
+		memset(dest, 0, str_size);
+		strcat(dest, path);
+		strcat(dest, ext);
 
-	if (cart->memory != NULL) {
-		fread(cart->memory, sizeof(u8), file_size, rom);
+		FILE* rom = fopen(dest, "r");
+		if (rom == NULL) {
+			printf("---Cartridge file: %s could not be found---\n", path);
+			return;
+		}
+		//Determine rom size
+		fseek(rom, 0, SEEK_END);
+		u32 file_size = ftell(rom);
+		fseek(rom, 0, SEEK_SET);
 
-		u16 header_start_offset = 0x7FF0;
+		cart->romsize = file_size;
+		//32kb, 64kb, 128kb, 256kb
+		if (file_size == CART_32K || file_size == CART_64K
+			|| file_size == CART_128K || file_size == CART_256K
+			|| file_size == CART_512K) {
+			cart->memory = (u8*)malloc(file_size * sizeof(u8));
+		}
+		else {
+			printf("--Cartridge size: 0x%05X not supported!--\n", file_size);
+			return;
+		}
 
-		u8 region_size = cart->memory[header_start_offset + 0xF];
-		cart->region = (region_size >> 4) & 0xF;
+		if (cart->memory != NULL) {
+			fread(cart->memory, sizeof(u8), file_size, rom);
+
+			u16 header_start_offset = 0x7FF0;
+
+			u8 region_size = cart->memory[header_start_offset + 0xF];
+			cart->region = (region_size >> 4) & 0xF;
+		}
+		fclose(rom);
+		free(dest);
+
+		//does the cart use sram?
+		char* sav_ext = ".sav";
+		dest = (u8*)malloc(str_size);
+
+		if (dest != NULL) {
+			s32 str_size = strlen(path) + strlen(sav_ext) + 1;
+			memset(dest, 0, str_size);
+			strcat(dest, path);
+			strcat(dest, sav_ext);
+
+			FILE* sav = fopen(dest, "r");
+			if (sav != NULL) {
+				cart->uses_sram = 1;
+				printf("cart uses sram\n");
+
+				fclose(sav);
+				free(dest);
+			}
+		}
 	}
-	fclose(rom);
 }
 
 void cartWriteU8(struct Cart* cart, u8 value, u32 address)
--- BlissSMS/Core/Cart.h
+++ BlissSMS/Core/Cart.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "Util.h"
 
-#define SRAM_SIZE 0x4000
 #define CART_32K 0x8000
 #define CART_64K 0x10000
 #define CART_128K 0x20000
@@ -10,14 +9,15 @@
 
 struct Cart {
 	u8* memory;
-	u8 ram_banks[0x8000]; //on board cartridge ram
-	u8 sram[SRAM_SIZE];
+	u8 ram_banks[0x8000]; //on board cartridge ram (sram)
 	u8 region;
 	u32 romsize;
+
+	u8 uses_sram;
 };
 
 void cartInit(struct Cart* cart);
-void cartLoad(struct Cart* cart, const char* path);
+void cartLoad(struct Cart* cart, char* path);
 
 void cartWriteU8(struct Cart* cart, u8 value, u32 address);
 u8 cartReadU8(struct Cart* cart, u32 address, u8 ram);
